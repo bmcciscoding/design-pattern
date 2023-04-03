e310bc3129b654f745cd323b9956698d56e4cc35
bmcciscoding <bmcciscoding@gmail.com>
2023-04-03 22:01:07 +0800

    add adapter

---
+++ typescrit/6_adapter.ts
@@ -0,0 +1,29 @@
+/// 接口协议
+interface Player {
+  playVideo(): string;
+}
+
+/// 具体内核
+class SysPlayerCore {
+  public play(): string {
+    return "Adaptee request";
+  }
+}
+
+/// 适配器
+class SysPlayerAdapter implements Player {
+  private readonly adaptee: SysPlayerCore;
+
+  constructor(adaptee: SysPlayerCore) {
+    this.adaptee = adaptee;
+  }
+
+  public playVideo(): string {
+    return this.adaptee.play();
+  }
+}
+
+const core = new SysPlayerCore();
+const player = new SysPlayerAdapter(core);
+
+console.log(player.playVideo()); // "Adaptee request"
